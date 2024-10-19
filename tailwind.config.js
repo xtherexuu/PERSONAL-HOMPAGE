43c4f5541c42d273e705b5c25f47d3568c2a9f25
Bartosz Załęski <bartosz.zaleski.pv@gmail.com>
2024-10-19 20:19:36 +0200

    added colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      "lbg": "#EDF2F4",
+      "dbg": "#2B2D42",
+      "vdbg": "#101010",
+      "gray": "#8D99AE",
+      "shine-red": "#EF233C",
+      "light-red": "#FF5555",
+      "light-orange": "#FF8855",
+      "light-yelow": "#E9C13F",
+    }
   },
   plugins: [],
 };
